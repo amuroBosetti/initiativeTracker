4943dc331fb9aaa6c2880c69266de8d50b8f0661
Mauro Bosetti <mauro.bosetti@10pines.com>
2023-06-08 20:41:52 -0300

    Imprime nombre reibido por request HTTP

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import pkg from 'johnny-five'
 import express from "express";
+import bodyParser from "body-parser";
+
 const { Board, LCD } = pkg;
 
 const board = new Board();
 
 const app = express();
-
+app.use(bodyParser.json())
 
 board.on('ready', () => {
     const lcd = new LCD({
@@ -19,7 +21,15 @@ board.on('ready', () => {
 
     app.get("/", (req, res) => {
         lcd.print("Holis");
-        res.send("OK");
+        res.status(200)
+        res.send();
+    });
+
+    app.post("/users", (req, res) => {
+        const { name, color, initiative } = req.body;
+        lcd.print(`${name}: ${initiative} - ${color}`);
+        res.status(200)
+        res.send();
     });
 
     board.repl.inject({
