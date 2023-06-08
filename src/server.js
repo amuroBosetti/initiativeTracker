3b41b4fb9d6edfa4cbf85d52d2afd72192bce530
Mauro Bosetti <mauro.bosetti@10pines.com>
2023-06-08 20:24:57 -0300

    Imprime por pantalla cuando se envia una request HTTP

--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 import pkg from 'johnny-five'
+import express from "express";
 const { Board, LCD } = pkg;
 
 const board = new Board();
+
+const app = express();
+
 
 board.on('ready', () => {
     const lcd = new LCD({
         controller: "PCF8574"
     });
     lcd.on()
-    lcd.print('holis')
+
+    app.listen(3000, () => {
+        console.log("Listening on port 3000")
+    })
+
+    app.get("/", (req, res) => {
+        lcd.print("Holis");
+        res.send("OK");
+    });
+
+    board.repl.inject({
+        lcd: lcd
+    });
 })
