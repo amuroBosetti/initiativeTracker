0a4852bf9402fd270b1cd0f79ffb56b74f0d8365
Mauro Bosetti <mauro.bosetti@10pines.com>
2023-06-14 18:01:28 -0300

    Cambia componente de clase a componente funcional

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./styles.css";
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Holis</h1>
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div>
+            <h1>Holis</h1>
+        </div>
+    );
 }
